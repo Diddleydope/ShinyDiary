66d60a4488ebdcdfac57236f13dd7aedd6e2e570
Samuel O'Neill <samueloporcelo@gmail.com>
2023-09-07 18:10:31 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts" context="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "firebase/app";
+    import { getAnalytics } from "firebase/analytics";
+    import { getAuth, initializeAuth } from "firebase/auth";
+    import { getFirestore } from "firebase/firestore";
+    // TODO: Add SDKs for Firebase products that you want to use
+    import { invoke } from '@tauri-apps/api/tauri';
+    import Login from './login.svelte'
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+    apiKey: "AIzaSyAFO-pDyfe65OKhoUMiyqCHT9tArwGkLyQ",
+    authDomain: "shinydiary-91a6f.firebaseapp.com",
+    projectId: "shinydiary-91a6f",
+    storageBucket: "shinydiary-91a6f.appspot.com",
+    messagingSenderId: "776903912155",
+    appId: "1:776903912155:web:69e404f8ed672da9c0fafe",
+    measurementId: "G-M3TE3FHMF6"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//make auth and firestore references
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+</script>
+
+
+<h1 class="title">Welcome to ShinyDiary!</h1>
+<Login></Login>
+
+
+<style>
+    .title{
+    margin-top: 30vh;
+    text-align: center;
+    }   
+</style>
+
---
+++ src/routes/login.svelte
@@ -0,0 +1,70 @@
+    <script>
+        import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+        import {db, auth} from './+page.svelte';
+        let email = "hello@gmail.com";
+        let password = "1234hello";
+
+        //Sign up the User
+        createUserWithEmailAndPassword(auth, email, password).then(cred => {
+            console.log("Hello LMAO");
+        })
+
+        //PROBLEM --> FUNCTION AUTOMATICALLY EXECUTES, WHY?? 
+    </script>
+    
+    
+    <div class="loginContainer">
+        <input  id= "emailInput" class="loginButtons" type="email" bind:value={email}/>
+        <br>
+        <input  id= "passwordInput" class= "loginButtons" type="password" bind:value={password}/>
+    </div>
+
+    <div class="buttonContainer">
+        <button class="Buttons" id="signin">Sign in</button>
+        <button class="Buttons" id="signup" 
+        on:click={() => createUserWithEmailAndPassword(auth,email,password)}>Sign up</button>
+    </div>
+    
+    
+    <style>
+        .loginContainer{
+            text-align: center;
+        }
+    
+        .loginButtons{
+            display: block;
+            scale: 2;
+            width: 15vw;
+            height: 2vh;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 0;
+        }
+    
+        #passwordInput{
+            margin-top: 1vh;
+        }
+
+        .buttonContainer{
+            display: flex;
+            justify-content: center;
+        }
+
+        .Buttons{
+            margin-top: 2vh;
+            width: 15vw;
+            height:4vh;
+            border-radius: 3%;
+            border-color: transparent;
+            background-color:cornsilk;
+        }
+
+        #signin{
+            margin-right: 0.25vw;
+        }
+
+        #signup{
+            margin-left: 0.25vw;
+        }
+    </style>
+    
